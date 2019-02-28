0c0375bc3a41c10c3f10478dc784241e19ae3398
TyCoding <2557988481@qq.com>
2019-02-28 10:39:56 +0800

    :pencil: init thymeleaf

--- spring-boot-thymeleaf/src/main/java/cn/tycoding/SpringBootThymeleafApplication.java
+++ spring-boot-thymeleaf/src/main/java/cn/tycoding/SpringBootThymeleafApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ThymeleafApplication {
+public class SpringBootThymeleafApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ThymeleafApplication.class, args);
+        SpringApplication.run(SpringBootThymeleafApplication.class, args);
     }
 
 }
