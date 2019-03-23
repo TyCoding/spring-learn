924de536fdc38b68b392b36dbf32fb823b903701
TyCoding <2557988481@qq.com>
2019-03-23 17:59:16 +0800

    :pencil: init boot-swagger

--- boot-swagger/src/main/java/cn/tycoding/BootSwaggerApplication.java
+++ boot-swagger/src/main/java/cn/tycoding/BootSwaggerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootSwaggerApplication {
+public class BootSwaggerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSwaggerApplication.class, args);
+		SpringApplication.run(BootSwaggerApplication.class, args);
 	}
 
 }
