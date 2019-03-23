aa186c23cceffeff54eeab0f9a833bebafed8e11
TyCoding <2557988481@qq.com>
2019-03-23 17:57:47 +0800

    :pencil: init boot-solr

--- boot-solr/src/main/java/cn/tycoding/BootSolrApplication.java
+++ boot-solr/src/main/java/cn/tycoding/BootSolrApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootSolrApplication {
+public class BootSolrApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootSolrApplication.class, args);
+        SpringApplication.run(BootSolrApplication.class, args);
     }
 
 }
--- boot-solr/src/test/java/cn/tycoding/BootSolrApplicationTests.java
+++ boot-solr/src/test/java/cn/tycoding/BootSolrApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootSolrApplicationTests {
+public class BootSolrApplicationTests {
 
     @Test
     public void contextLoads() {
