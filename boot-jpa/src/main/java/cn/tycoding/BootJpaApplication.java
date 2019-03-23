416475c992b937168110cd1b2ef320dfd83883c9
TyCoding <2557988481@qq.com>
2019-03-23 17:27:21 +0800

    :pencil: init jpa

--- boot-jpa/src/main/java/cn/tycoding/BootJpaApplication.java
+++ boot-jpa/src/main/java/cn/tycoding/BootJpaApplication.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class SpringBootJpaApplication {
+public class BootJpaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootJpaApplication.class, args);
+        SpringApplication.run(BootJpaApplication.class, args);
     }
 
 }
--- boot-jpa/src/test/java/cn/tycoding/BootJpaApplicationTests.java
+++ boot-jpa/src/test/java/cn/tycoding/BootJpaApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootJpaApplicationTests {
+public class BootJpaApplicationTests {
 
     @Test
     public void contextLoads() {
