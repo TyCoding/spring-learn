ee5c1e038c98f480e319066d95ff43f872cb6b03
TyCoding <2557988481@qq.com>
2019-04-23 14:29:48 +0800

    :pencil: init boot-security-oauth2

--- boot-security-oauth2/src/main/java/cn/tycoding/config/ResourceServerConfig.java
+++ boot-security-oauth2/src/main/java/cn/tycoding/config/ResourceServerConfig.java
@@ -1,6 +1,7 @@
 package cn.tycoding.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -14,4 +15,16 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/hello")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+
+                .and()
+                .csrf().disable();
+    }
 }
