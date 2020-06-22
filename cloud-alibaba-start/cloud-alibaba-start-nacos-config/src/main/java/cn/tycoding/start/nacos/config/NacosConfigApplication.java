9c69da649846f1b29b5451283fc6735fff3dc992
TyCoding <tytumo@163.com>
2020-06-22 21:42:43 +0800

    init cloud-alibaba-start

---
+++ cloud-alibaba-start/cloud-alibaba-start-nacos-config/src/main/java/cn/tycoding/start/nacos/config/NacosConfigApplication.java
@@ -0,0 +1,20 @@
+package cn.tycoding.start.nacos.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 演示Nacos配置中心客户端端
+ *
+ * @author tycoding
+ * @date 2020/6/22
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigApplication.class, args);
+    }
+}
---
+++ cloud-alibaba-start/cloud-alibaba-start-nacos-config/src/main/java/cn/tycoding/start/nacos/config/controller/ConfigController.java
@@ -0,0 +1,23 @@
+package cn.tycoding.start.nacos.config.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tycoding
+ * @date 2020/6/22
+ */
+@RestController
+public class ConfigController {
+
+    // 从Spring上下文环境中获取配置信息，保证配置实时更新
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
+    @GetMapping("/config")
+    public String config() {
+        return applicationContext.getEnvironment().getProperty("config.author");
+    }
+}
